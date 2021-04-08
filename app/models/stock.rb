498fe41a00e5ddde0104cef2462d36cf92d2aada
koji_yamamoto <koji_ko428@yahoo.co.jp>
2021-04-08 14:03:42 +0000

    stock バリデーション（簡易）実装

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
 class Stock < ApplicationRecord
+  validates :item_name, presence: true
+  validates :content, presence: true
+
+  validates :item_name, length: { in: 1..30 }
+  validates :content, length: { in: 1..140 }
+
+
 end
